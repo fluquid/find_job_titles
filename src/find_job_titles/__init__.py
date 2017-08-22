3e8894bbe48abd01cededa8052112504ccd1f5d5
Johannes Ahlmann <johannes@fluquid.com>
2017-08-22 16:46:03 +0100

    longest matches, pyahocorasick
    - completely rewrote longest matches code
    - added lots of unit tests for longest matches
    - implemented pyahocorasick solution in addition to acora

--- src/find_job_titles/__init__.py
+++ src/find_job_titles/__init__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 """
-Module for find_job_titles library.
+The `find_job_titles` library finds mentions of job titles in strings.
+
+In order to do so it compiles a search datastructure (Aho Corasick) and uses
+    a precompiled list of >70k job titles as a reference list.
+
+It returns the longest matching job title, including cross-overlapping matches,
+    together with the start and end position in the given string.
 
 TODO:
-* startup time is far too long; need to reduce size of job titles file
-* sort job titles by corpus frequency, so that we can load only top-N titles
-* ideally test job titles against a real-world corpus to weed out irrelevant
- titles
-* function to return only longest match?
+* also compare to https://github.com/scrapinghub/webstruct/blob/master/webstruct/utils.py#L155
 """
 
 __author__ = 'Johannes Ahlmann'
@@ -18,12 +20,19 @@ __version__ = '0.5.0'
 import gzip
 from pkg_resources import resource_stream
 import logging
-from itertools import groupby
+from collections import namedtuple
 
 from acora import AcoraBuilder
+import ahocorasick
+
+
+Match = namedtuple('Match', ['start', 'end', 'match'])
 
 
 def load_titles():
+    """
+    load job titles as generator from txt.gz file included in the library
+    """
     with resource_stream('find_job_titles',
                          'data/titles_combined.txt.gz') as fhandle:
         with gzip.GzipFile(fileobj=fhandle, mode='r') as gzf:
@@ -32,34 +41,118 @@ def load_titles():
                 yield line.decode('utf-8').strip()
 
 
-class FinderAcora(object):
+def longest_match(matches):
+    """
+    find respective longest matches from all overlapping aho corasick matches
+    """
+    longest = next(matches)
+    if longest is None:
+        return
+
+    for elt in matches:
+        # if (a contains b) or (b contains a)
+        if (    (elt.start >= longest.start and elt.end <= longest.end) or
+                (longest.start >= elt.start and longest.end <= elt.end)):
+            longest = max(longest, elt, key=lambda x: x.end - x.start)
+        else:
+            yield longest
+            longest = elt
+    yield longest
+
+
+def add_start(matches):
+    """
+    convert acora `(match, start)` tuples into `Match(start, end, match)` format
+    """
+    return (Match(start=start, end=start + len(match), match=match)
+            for match, start in matches)
+
+
+class BaseFinder(object):
+    """
+    Base class containing query methods
+    """
+    def findall(self, string, use_longest=True):
+        """
+        utility function returning `list` of results from `finditer`
+        :param string: string to search target patterns in
+        :param use_longest: if True only return longest matches,
+                            else return all overlapping matches
+        :returns: list of matches of type `Match`
+        """
+        return list(self.finditer(string, use_longest=use_longest))
+
+    def finditer(self, string, use_longest=True):
+        """
+        iterator of all (longest) matches of target patterns in `string`
+        :param string: string to search target patterns in
+        :param use_longest: if True only return longest matches,
+                            else return all overlapping matches
+        :returns: generator of matches of type `Match`
+        """
+        if use_longest:
+            return longest_match(self.find_raw(string))
+        else:
+            return self.find_raw(string)
+
+
+class FinderAcora(BaseFinder):
     """
     Finder class based on "acora" library.
 
-    building data structure takes a moment, and having issues
-        with finding longest matches.
+    Note: Building data structure seems to be significantly slower than with
+          pyahocorasick
     """
-    def __init__(self, use_unicode=True, ignore_case=False):
-        titles = (load_titles()
+    def __init__(self, use_unicode=True, ignore_case=False, titles=None):
+        """
+        :param use_unicode: whether to use `titles` as unicode or bytestrings
+        :param ignore_case: if True ignore case in all matches
+        :param titles: if given, overrides default `load_titles()` values
+        """
+        titles = titles if titles else load_titles()
+        titles = (titles
                   if use_unicode
-                  else (s.encode('ascii') for s in load_titles()))
+                  else (s.encode('ascii') for s in titles))
         builder = AcoraBuilder()
         logging.info('building job title searcher')
         builder.update(titles)
         self.ac = builder.build(ignore_case=ignore_case)
         logging.info('building done')
 
-    def search_longest(self, matches):
-        # source: https://github.com/scoder/acora#faqs-and-recipes
-        for pos, match_set in groupby(matches, lambda x: len(x[0]) + x[1]):
-            yield max(match_set, key=lambda x: len(x[0]))
-
-    def findall(self, string):
-        return list(self.search_longest(self.ac.findall(string)))
-
-    def finditer(self, string):
-        return self.search_longest(self.ac.finditer(string))
+    def find_raw(self, string):
+        """
+        generator of raw, overlapping matches of all lengths from automaton
+        """
+        return add_start(self.ac.finditer(string))
 
 
-class Finder(FinderAcora):
-    pass
+class FinderPyaho(BaseFinder):
+    """
+    Finder class based on "pyahocorasick" library.
+
+    TODO:
+    - use pickle and unpickle support for `self.autom`
+    """
+    def __init__(self, titles=None):
+        """
+        :param titles: if given, overrides default `load_titles()` values
+        """
+        titles = titles if titles else load_titles()
+        logging.info('building job title searcher')
+        autom = ahocorasick.Automaton()
+        for title in titles:
+            autom.add_word(title, title)
+        autom.make_automaton()
+        self.autom = autom
+        logging.info('building done')
+
+    def find_raw(self, string):
+        """
+        generator of raw, overlapping matches of all lengths from automaton
+        """
+        for end, match in self.autom.iter(string):
+            start = end - len(match) + 1
+            yield Match(start=start, end=end, match=match)
+
+
+Finder = FinderPyaho
--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,38 +1,96 @@
 from __future__ import unicode_literals
-from find_job_titles import Finder
+import pytest
 
-# FIXME: should be in setup code
-finder = Finder()
+from find_job_titles import FinderAcora, FinderPyaho
+
+# using test titles to reduce test time for acora
+TEST_TITLES = ['Senior Vice President', 'Vice President', 'President',
+               'President & CEO']
+FINDERS = [FinderAcora(TEST_TITLES), FinderPyaho()]
 
 
-def test_some_matches():
-    fds = [x[0] for x in finder.finditer('I am the Senior Vice President')]
+@pytest.fixture(params=FINDERS)
+def finder(request):
+    return request.param
+
+
+def test_some_matches(finder):
+    fds = [x.match for x in finder.finditer('I am the Senior Vice President')]
     assert fds == ['Senior Vice President']
 
-    fds = [x[0] for x in finder.finditer('I am the Senior Vice President asdf')]
+    fds = [x.match for x in finder.finditer('I am the Senior Vice President asdf')]
     assert fds == ['Senior Vice President']
 
-    fds = [x[0] for x in finder.finditer('Senior Vice President I am')]
+    fds = [x.match for x in finder.finditer('Senior Vice President I am')]
     assert fds == ['Senior Vice President']
 
 
-def test_no_matches():
+def test_no_matches(finder):
     assert not finder.findall('defo no matches in here')
 
 
-def test_position():
-    fds = [x[1] for x in finder.findall('at the end: Vice President')]
+def test_position(finder):
+    fds = [x.start for x in finder.findall('at the end: Vice President')]
     assert fds == [12]
 
-    fds = [x[1] for x in finder.findall(
+    fds = [x.start for x in finder.findall(
         'in the middle Vice President of sentence')]
     assert fds == [14]
 
-    fds = [x[1] for x in finder.findall('Vice President at the beginning')]
+    fds = [x.start for x in finder.findall('Vice President at the beginning')]
     assert fds == [0]
 
 
-def test_overlapping():
-    fds = [x[0] for x in finder.findall('Vice President & CEO')]
+def test_overlapping(finder):
+    fds = [x.match for x in finder.findall('Vice President & CEO')]
     assert fds == ['Vice President', 'President & CEO']
 
+
+def test_overlaps(finder):
+    # leading
+    FinderClass = type(finder)
+    finder = FinderClass(titles=['a', 'ab', 'abc'])
+    fds = [x.match for x in finder.finditer('xxxabcde')]
+    assert fds == ['abc'], fds
+    fds = [x.match for x in finder.finditer('xxxabc')]
+    assert fds == ['abc'], fds
+    fds = [x.match for x in finder.finditer('abc')]
+    assert fds == ['abc'], fds
+
+    # trailing
+    finder = FinderClass(titles=['abc', 'bc', 'b'])
+    fds = [x.match for x in finder.finditer('xxxabcde')]
+    assert fds == ['abc'], fds
+    fds = [x.match for x in finder.finditer('xxxabc')]
+    assert fds == ['abc'], fds
+
+    # middling
+    finder = FinderClass(titles=['abcd', 'bc'])
+    fds = [x.match for x in finder.finditer('xxxabcd')]
+    assert fds == ['abcd'], fds
+    fds = [x.match for x in finder.finditer('xxxabcde')]
+    assert fds == ['abcd'], fds
+
+    # cross overlap
+    finder = FinderClass(titles=['abc', 'bcd'])
+    fds = [x.match for x in finder.finditer('xxxabcd')]
+    assert fds == ['abc', 'bcd'], fds
+    fds = [x.match for x in finder.finditer('xxxabcde')]
+    assert fds == ['abc', 'bcd'], fds
+    fds = [x.match for x in finder.finditer('abcd')]
+    assert fds == ['abc', 'bcd'], fds
+
+    # no overlap
+    finder = FinderClass(titles=['abc', 'efg'])
+    fds = [x.match for x in finder.finditer('xxxabcdefg')]
+    assert fds == ['abc', 'efg'], fds
+
+    # no match
+    finder = FinderClass(titles=['abc', 'efg'])
+    fds = [x.match for x in finder.finditer('xxx')]
+    assert fds == [], fds
+
+    # empty string
+    finder = FinderClass(titles=['abc', 'efg'])
+    fds = [x.match for x in finder.finditer('')]
+    assert fds == [], fds
